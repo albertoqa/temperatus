44e7ea09cd55e77c39e30781fbd21e8e59251ee2
Alberto Quesada <qa.alberto@gmail.com>
2016-01-27 21:04:36 +0100

    TableView for games saved in DB. Save new games.

--- src/main/java/temperatus/controller/NewGameController.java
+++ src/main/java/temperatus/controller/NewGameController.java
@@ -2,8 +2,13 @@ package temperatus.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import temperatus.model.pojo.Game;
+import temperatus.model.service.GameService;
 import temperatus.util.Animation;
 
 import java.net.URL;
@@ -17,9 +22,26 @@ public class NewGameController implements Initializable {
 
     @FXML
     TitledPane titledPane;
+    @FXML
+    TextField nameInput;
+    @FXML
+    TextArea observationsInput;
+    @FXML
+    TextField numButtonsInput;
+
+    @Autowired
+    GameService gameService;
 
     @FXML
-    private void saveGame(){}
+    private void saveGame(){
+        // TODO hacer todas las comprobaciones necesarias
+        Game game = new Game();
+        game.setTitle(nameInput.getText());
+        game.setObservations(observationsInput.getText());
+        game.setNumButtons(Integer.parseInt(numButtonsInput.getText()));
+
+        gameService.save(game);
+    }
 
     @FXML
     private void cancel() {
--- src/main/java/temperatus/controller/configuration/GamesGeneralController.java
+++ src/main/java/temperatus/controller/configuration/GamesGeneralController.java
@@ -1,8 +1,16 @@
 package temperatus.controller.configuration;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import temperatus.model.pojo.Game;
+import temperatus.model.service.GameService;
 import temperatus.util.Constants;
 import temperatus.util.VistaNavigator;
 
@@ -16,12 +24,31 @@ import java.util.ResourceBundle;
 public class GamesGeneralController implements Initializable {
 
     @FXML
+    TableView gamesTable;
+
+    @Autowired
+    GameService gameService;
+
+    @FXML
     private void newGame() {
         VistaNavigator.openModal(Constants.NEW_GAME, "New Game");
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<Game> data = FXCollections.observableArrayList(gameService.getAll());
+        TableColumn name = new TableColumn("Name");
+        TableColumn observations = new TableColumn("Observations");
+
+        name.setCellValueFactory(
+                new PropertyValueFactory<Game,String>("title")
+        );
+        observations.setCellValueFactory(
+                new PropertyValueFactory<Game,String>("observations")
+        );
+
+        gamesTable.setItems(data);
+        gamesTable.getColumns().addAll(name, observations);
 
     }
 }
