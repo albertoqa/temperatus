20ccc23d9977b9553f845f9f83251a8eb02f7052
Alberto Quesada <qa.alberto@gmail.com>
2016-02-14 12:22:05 +0100

    manage general and subject fxml

--- src/main/java/temperatus/controller/manage/ManageController.java
+++ src/main/java/temperatus/controller/manage/ManageController.java
@@ -1,7 +1,34 @@
 package temperatus.controller.manage;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import temperatus.controller.AbstractController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by alberto on 14/2/16.
  */
-public class ManageController {
+@Controller
+public class ManageController implements Initializable, AbstractController {
+
+    @FXML private Label manageLabel;
+    @FXML private TabPane tabPane;
+
+    static Logger logger = Logger.getLogger(ManageController.class.getName());
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @Override
+    public void translate() {
+
+    }
 }
