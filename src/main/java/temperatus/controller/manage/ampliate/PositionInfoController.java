0fc4550d8946932a01695a496ae2686609e2ddf3
Alberto Quesada <qa.alberto@gmail.com>
2016-03-02 23:38:09 +0100

    extends abstract and implements translate

--- src/main/java/temperatus/controller/manage/ampliate/PositionInfoController.java
+++ src/main/java/temperatus/controller/manage/ampliate/PositionInfoController.java
@@ -1,7 +1,14 @@
 package temperatus.controller.manage.ampliate;
 
+import temperatus.controller.AbstractController;
+
 /**
  * Created by alberto on 29/2/16.
  */
-public class PositionInfoController {
+public class PositionInfoController implements AbstractController {
+
+    @Override
+    public void translate() {
+
+    }
 }
