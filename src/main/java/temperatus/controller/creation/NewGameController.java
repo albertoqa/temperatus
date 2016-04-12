5d481f05b0358dcd9050cb2266e59f3fde686842
Alberto Quesada <qa.alberto@gmail.com>
2016-04-12 11:21:31 +0200

    Added a 2d human body to new game. The user can select click on the image and a circle and a number will be draw.

--- src/main/java/temperatus/controller/creation/NewGameController.java
+++ src/main/java/temperatus/controller/creation/NewGameController.java
@@ -1,11 +1,20 @@
 package temperatus.controller.creation;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import org.controlsfx.control.CheckListView;
 import org.controlsfx.control.ListSelectionView;
 import org.hibernate.exception.ConstraintViolationException;
@@ -24,7 +33,9 @@ import temperatus.model.service.PositionService;
 import temperatus.util.Constants;
 import temperatus.util.VistaNavigator;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -35,8 +46,6 @@ import java.util.ResourceBundle;
 @Scope("prototype")
 public class NewGameController extends AbstractCreationController implements Initializable {
 
-    // TODO add default positions to the Game
-
     @FXML Label nameLabel;
     @FXML Label observationsLabel;
     @FXML Label numButtonsLabel;
@@ -45,6 +54,11 @@ public class NewGameController extends AbstractCreationController implements Ini
     @FXML TextArea observationsInput;
     @FXML TextField numButtonsInput;
 
+    @FXML StackPane imageStack;
+    @FXML ImageView imageView;
+    @FXML Canvas canvas;
+    @FXML TextField drawNumber;
+
     @FXML ListSelectionView<Position> positionsSelector;
     @FXML CheckListView<Formula> formulasList;
 
@@ -52,16 +66,44 @@ public class NewGameController extends AbstractCreationController implements Ini
     @Autowired PositionService positionService;
     @Autowired FormulaService formulaService;
 
+    private List<Image> images;
+    private int selectedImage;
+    private GraphicsContext gc;
+
+    private final int radius = 4;
+    private boolean drawed = false;
+
     static Logger logger = LoggerFactory.getLogger(NewGameController.class.getName());
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        images = new ArrayList<>();
+        images.add(new Image("/images/frontBody.png"));
+        images.add(new Image("/images/backBody.png"));
+
+        imageView.setImage(images.get(0));
+        selectedImage = 0;
+
+        canvas.setOnMouseClicked(mouseHandler);
+        gc = canvas.getGraphicsContext2D();
 
         positionsSelector.getSourceItems().addAll(positionService.getAll());
         formulasList.getItems().addAll(formulaService.getAll());
 
         translate();
     }
+
+    private EventHandler<MouseEvent> mouseHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+            if (mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED) {
+                gc.fillOval(mouseEvent.getX()-radius, mouseEvent.getY()-radius, radius, radius);
+                gc.fillText(drawNumber.getText(), mouseEvent.getX(), mouseEvent.getX());
+                drawed = true;
+            }
+        }
+    };
 
     @Override
     @FXML
@@ -108,6 +150,43 @@ public class NewGameController extends AbstractCreationController implements Ini
             logger.warn("Unknown exception" + ex.getMessage());
             showAlert(Alert.AlertType.ERROR, "Unknown error.");
         }
+    }
+
+    private void keepImage() {
+        if(drawed) {
+            WritableImage snapshot = imageStack.snapshot(new SnapshotParameters(), null);
+            images.set(selectedImage, snapshot);
+            clearCanvas();
+            drawed = false;
+        }
+    }
+
+    @FXML
+    private void imageLeft() throws IOException {
+        keepImage();
+        selectedImage = (selectedImage - 1) % images.size();
+        if (selectedImage < 0) {
+            selectedImage = -selectedImage;
+        }
+        imageView.setImage(images.get(selectedImage));
+    }
+
+    @FXML
+    private void imageRight() {
+        keepImage();
+        selectedImage = (selectedImage + 1) % images.size();
+        imageView.setImage(images.get(selectedImage));
+    }
+
+    private void saveImages() {
+
+
+    }
+
+    @FXML
+    private void clearCanvas() {
+        // TODO change to reload image
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
     }
 
     @Override
