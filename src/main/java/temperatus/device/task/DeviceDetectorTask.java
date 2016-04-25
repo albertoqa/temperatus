6dfc39ecb6599c91e633f5a0e3f2fa1975ee6568
Alberto Quesada <qa.alberto@gmail.com>
2016-04-25 11:48:16 +0200

    fix on generalInfo fxml and revert of device detector task

--- src/main/java/temperatus/device/task/DeviceDetectorTask.java
+++ src/main/java/temperatus/device/task/DeviceDetectorTask.java
@@ -4,7 +4,6 @@ import com.dalsemi.onewire.OneWireAccessProvider;
 import com.dalsemi.onewire.OneWireException;
 import com.dalsemi.onewire.adapter.DSPortAdapter;
 import com.dalsemi.onewire.container.OneWireContainer;
-import javafx.util.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +30,6 @@ public class DeviceDetectorTask implements Runnable {
     private static Logger logger = LoggerFactory.getLogger(DeviceDetectorTask.class.getName());
 
     private List<String> serialsDetected = new ArrayList<>();   // list of device's serials currently connected (already detected)
-    private List<Pair<String, String>> devicesDetected = new ArrayList<>();
 
     @Autowired DeviceDetectorSource deviceDetectorSource;   // create event to let all listeners know about what is happening
     @Autowired DeviceSemaphore deviceSemaphore;             // shared semaphore
@@ -67,52 +65,30 @@ public class DeviceDetectorTask implements Runnable {
 
                 try {
                     adapter.selectPort(port_name);
-                    String adapterNamePort = adapter.getAdapterName() + "-" + adapter.getPortName();
                     if (adapter.adapterDetected()) {
 
                         // Check if devices previously detected are still connected
-                        /*for (String serial : serialsDetected) {
+                        for (String serial : serialsDetected) {
                             if (!adapter.isPresent(serial)) {
                                 serialsDetected.remove(serial);
                                 deviceDetectorSource.departureEvent(serial);
                             }
-                        }*/
+                        }
 
                         adapter.beginExclusive(true);
                         adapter.setSearchAllDevices();
                         adapter.targetAllFamilies();
-                        for (Enumeration containers = adapter.getAllDeviceContainers(); containers.hasMoreElements(); ) {
-                            OneWireContainer container = (OneWireContainer) containers.nextElement();
-                            String serial = container.getAddressAsString();
+                        for (Enumeration ibutton_enum = adapter.getAllDeviceContainers(); ibutton_enum.hasMoreElements(); ) {
+                            OneWireContainer ibutton = (OneWireContainer) ibutton_enum.nextElement();
+                            String serial = ibutton.getAddressAsString();
 
                             // New device detected, add it to the list and warn the listeners
-                            /*if (!serialsDetected.contains(serial)) {
+                            if (!serialsDetected.contains(serial)) {
                                 serialsDetected.add(serial);
-                                deviceDetectorSource.arrivalEvent(container, adapter.getAdapterName(), adapter.getPortName());
-                            }*/
-
-                            for(Pair<String, String> device: devicesDetected) {
-                                if(!adapter.isPresent(device.getKey())) {
-                                    serialsDetected.remove(device.getKey());
-                                    deviceDetectorSource.departureEvent(device.getKey());
-                                }
+                                deviceDetectorSource.arrivalEvent(ibutton, adapter.getAdapterName(), adapter.getPortName());
                             }
-
-                            Pair<String, String> device = new Pair<>(serial, adapterNamePort);
-                            if(!devicesDetected.contains(device)) {
-                                devicesDetected.add(device);
-                                deviceDetectorSource.arrivalEvent(container, adapter.getAdapterName(), adapter.getPortName());
-                            }
-
                         }
                         adapter.endExclusive();
-                    } else {
-                        /*for(Pair<String, String> device: devicesDetected) {
-                            if(device.getValue().equals(adapterNamePort)) {
-
-                            }
-                            // este adaptador no esta conectado... si tenía dispositivos conectados a el esos el programa lso reconoce como conectados
-                        }*/
                     }
                     adapter.freePort();
 
