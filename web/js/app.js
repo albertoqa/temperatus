3b6ce3ce2a225282e63d2dc7bbb1122e5c213bff
Alberto Quesada <qa.alberto@gmail.com>
2016-06-11 12:15:47 +0200

    Refactor html code to main view. ScrollSpy works on the new view and contact form validation improved

--- web/js/app.js
+++ web/js/app.js
@@ -19,14 +19,41 @@
 })();
 
 (function () {
-
   angular
     .module('temperatusApp')
     .controller('mainCtrl', mainCtrl);
 
-  mainCtrl.$inject = ['$scope'];
-  function mainCtrl ($scope) {
+  mainCtrl.$inject = ['$scope', '$http'];
+  function mainCtrl ($scope, $http) {
     var vm = this;
-  }
 
+    vm.credentials = {
+      name : "",
+      _replyto : "",
+      comment : ""
+    };
+
+    vm.onSubmit = function () {
+      vm.formError = "";
+      if (!vm.credentials.name) {
+        vm.formError = "All fields required, please try again.";
+        return false;
+      } else if(!vm.credentials._replyto) {
+        vm.formError = "Incorrect email address, please try again.";
+        return false;
+      } else if(!vm.credentials.comment) {
+        vm.formError = "Comments are mandatory, please try again.";
+        return false;
+      }else {
+        vm.doSubmit();
+      }
+    };
+
+    vm.doSubmit = function() {
+      vm.formError = "";
+      vm.formSuccess = "Thanks. We will get back to you as soon as possible."
+      console.log(vm.credentials);
+      $http.post('https://formspree.io/qa.alberto@gmail.com', vm.credentials);
+    };
+  }
 })();
