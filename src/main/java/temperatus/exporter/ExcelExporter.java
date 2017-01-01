497cd06262f372dcde4d7b4a0a5c3dab2858a80a
Alberto Quesada <qa.alberto@gmail.com>
2017-01-01 23:34:16 +0100

    Getting ready for the development of the feature 4: use templates for exporting.

---
+++ src/main/java/temperatus/exporter/ExcelExporter.java
@@ -0,0 +1,21 @@
+package temperatus.exporter;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ * Created by alberto on 01/01/2017.
+ */
+public class ExcelExporter extends AbstractExporter {
+
+
+    public XSSFWorkbook export(String templatePath) {
+
+
+
+
+
+        return null;
+    }
+
+
+}
