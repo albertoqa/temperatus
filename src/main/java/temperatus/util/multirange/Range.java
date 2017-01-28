158b52be273c32559afe8c1d7eeeb7202075c297
Alberto Quesada <qa.alberto@gmail.com>
2017-01-28 17:48:58 +0100

    Added code of multiRangeControl, just temporally.

---
+++ src/main/java/temperatus/util/multirange/Range.java
@@ -0,0 +1,47 @@
+package temperatus.util.multirange;
+
+/**
+ * Define a new range with an id, a low and a high values.
+ * <p>
+ * Created by alberto on 09/01/2017.
+ */
+public class Range {
+
+    private int id;
+    private Double low;
+    private Double high;
+
+    Range(int id, Double low, Double high) {
+        this.id = id;
+        this.low = low;
+        this.high = high;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getLow() {
+        return low;
+    }
+
+    public void setLow(Double low) {
+        this.low = low;
+    }
+
+    public Double getHigh() {
+        return high;
+    }
+
+    public void setHigh(Double high) {
+        this.high = high;
+    }
+
+    public double getAmplitude() {
+        return high - low;
+    }
+}
